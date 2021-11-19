85f1debd2243487264297fc602cb05d2072592f1
Josh Campbell <josh@p4a.net>
2021-11-18 21:24:11 -0800

    adds operation arg

--- blog_to_gists.py
+++ blog_to_gists.py
@@ -4,6 +4,8 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("blog", help="List of all files in ./blogs to upload to Gists.")
+parser.add_argument("operation", help="create or update")
+parser.add_argument(
+    "blog", help="Space delimited list of all files to upload to Gists.")
 args = parser.parse_args()
-print("Ok we gotttt\n ", args.blog)
+print("Ok we gotttt\n {}\n {}".format(args.operation, args.blog))
