39e277edd0e964a15980719e64ba46912258f806
Josh Campbell <josh@p4a.net>
2021-11-19 09:17:44 -0800

    creation flow :fire:

--- blog_to_gists.py
+++ blog_to_gists.py
@@ -3,8 +3,9 @@
 
 import argparse
 import os
-import requests
 import sys
+from pathlib import Path
+from github import Github, Gist, InputFileContent
 
 
 token = os.environ.get('GISTS_TOKEN')
@@ -14,7 +15,17 @@ parser.add_argument("operation", help="create or update")
 parser.add_argument(
     "blog", help="Space delimited list of all files to upload to Gists.")
 args = parser.parse_args()
-print("Ok we gotttt\n {}\n {}".format(args.operation, args.blog))
+print("Blog posts to process\n {}\n  {}".format(args.operation, args.blog))
 
-r = requests.post('https://api.github.com/gists',
-                  data={'key': 'value'})
+g = Github(token)
+github_user = g.get_user()
+
+for post in args.blog.split(","):
+    print("Processing {}".format(post))
+    if args.operation == "create":
+        d = {}
+        d[post.split("/")[1]] = InputFileContent(Path(post).read_text())
+        new_post = github_user.create_gist(True, d, post)
+        print(" {}: {}".format(post, new_post.id))
+    elif args.operation == "update":
+        print("todo")
